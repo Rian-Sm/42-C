5997124a02074f795131164bc0fa8fa3edb4cdfd
rian-sm <riansm100@gmail.com>
2025-04-19 22:09:00 -0300

    normalizado ex0{0-5}

--- 42-piscina-c-00/ex03/ft_print_numbers.c
+++ 42-piscina-c-00/ex03/ft_print_numbers.c
@@ -6,17 +6,20 @@
 /*   By: riasanto <riasanto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:21:12 by riasanto          #+#    #+#             */
-/*   Updated: 2025/04/15 11:14:15 by riasanto         ###   ########.fr       */
+/*   Updated: 2025/04/17 11:06:49 by riasanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_numbers(void){
-	char charNumbers = '0';
+void	ft_print_numbers(void)
+{
+	char	numbers;
 
-	while(charNumbers<='9'){
-		write(1, &charNumbers, 1);
-		charNumbers++;
+	numbers = '0';
+	while (numbers <= '9')
+	{
+		write(1, &numbers, 1);
+		numbers++;
 	}
 }
